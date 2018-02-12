3811506515303456dff7c5edc2c0325e4d181a8c
Éderson Tiago Szlachta <ederson@pc1.edtsz.com>
2018-02-12 15:01:59 -0200

    Fix a bug with filename containing whitespaces, quotes etc

--- srt2vtt.sh
+++ srt2vtt.sh
@@ -13,30 +13,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-if ! [ -f "$1" ]; then
- 	echo 'File not found'
- 	exit 1
-fi
-
 dos2unix --help foo > /dev/null 2>&1 || { echo >&2 "dos2unix is required but it's not installed. Aborting."; exit 1; }
 uchardet --help foo > /dev/null 2>&1 || { echo >&2 "uchardet is required but it's not installed. Aborting."; exit 1; }
 iconv    --help foo > /dev/null 2>&1 || { echo >&2 "iconv is required but it's not installed. Aborting."; exit 1; }
 sed      --help foo > /dev/null 2>&1 || { echo >&2 "sed is required but it's not installed. Aborting."; exit 1; }
 
-src=$1
-dst="${src%.*}.vtt"
+names=( "$@" )
+for src in "${names[@]}"
+do
+	if ! [ -f "$src" ]; then
+		echo "File not found: $src"
+		exit 1
+	fi
 
-# Convert to UTF-8
-iconv -f "$(uchardet $src)" -t UTF-8 $src -o $dst
+	dst="${src%.*}.vtt"
 
-# Convert CRLF to LF
-dos2unix -q $dst
+	# Convert to UTF-8
+	iconv -f $(uchardet "$src" | sed 's#^x-mac-#mac#') -t UTF-8 "$src" -o "$dst"
 
-# Trim white space start and end
-sed -i 's/^[ \t]*//;s/[ \t]*$//' $dst
+	# Convert CRLF to LF
+	dos2unix -q "$dst"
 
-# Convert time strings to VTT pattern
-sed -i -E 's/(([0-9]{2}:){2}[0-9]{2}),([0-9]{3})/\1\.\3/g' $dst
+	# Trim white space start and end
+	sed -i 's/^[ \t]*//;s/[ \t]*$//' "$dst"
 
-# Add HEADERS
-sed -i '1iWEBVTT\n' $dst
+	# Convert time strings to VTT pattern
+	sed -i -E 's/(([0-9]{2}:){2}[0-9]{2}),([0-9]{3})/\1\.\3/g' "$dst"
+
+	# Add HEADERS
+	sed -i '1iWEBVTT\n' "$dst"
+
+	echo "Done: $dst"
+done
